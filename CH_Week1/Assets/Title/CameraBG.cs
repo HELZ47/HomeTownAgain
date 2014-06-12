e32536237b250f54f6f99acb5036c1e7e15d2d2a
Winnie Song <w.jy.song@gmail.com>
2014-06-12 09:03:56 -0400

    Temp Title & Depth of Field script

---
+++ CH_Week1/Assets/Title/CameraBG.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBG : MonoBehaviour {
+    public Color color1 = Color.red;
+    public Color color2 = Color.blue;
+    public float duration = 10.0F;
+    void Update() {
+        float t = Mathf.PingPong(Time.time/5, duration) / duration;
+        camera.backgroundColor = Color.Lerp(color1, color2, t);
+    }
+    // void Example() {
+    //     camera.clearFlags = CameraClearFlags.SolidColor;
+    // }
+}
