698788de91ce9cb6abafe0c88115329117f1c24b
Li Zhi He <hlz1991@hotmail.com>
2014-06-12 15:23:11 -0400

    title scene now load into demo scene after 1 second

---
+++ CH_Week1/Assets/Title/LevelLoader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.timeSinceLevelLoad > 1f) {
+//			Application.LoadLevel("demo");
+			Application.LoadLevel ("demo");
+		}
+
+//		if(Application.GetStreamProgressForLevel("demo") ==1){
+//			Application.LoadLevel("demo");
+//		}
+
+	}
+}
