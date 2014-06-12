31c63b6ae5c25f9f47cc52a90e884042abb19e70
joshr2121 <joshr2121@gmail.com>
2014-06-12 15:12:29 -0400

    Implemented font for real

--- CH_Week1/Assets/Scripts/OnCameraMessage.cs
+++ CH_Week1/Assets/Scripts/OnCameraMessage.cs
@@ -4,13 +4,28 @@ using System.Collections;
 public class OnCameraMessage : MonoBehaviour {
 
 	public GUIText text;
+	
+	public bool appearing;
+	public float fadeRate;
 
 	// Use this for initialization
 	void Start () {
+		appearing = false;
+		fadeRate = 0.1f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		text.pixelOffset = new Vector2 (Screen.width/2, 0);
+		if (appearing && text.color.a < 255) {
+			Color textColor = text.color;
+			textColor.a += fadeRate;
+			text.color = textColor;
+		}
+		else if (!appearing && text.color.a > 0) {
+			Color textColor = text.color;
+			textColor.a -= fadeRate;
+			text.color = textColor;
+		}
 	}
 }
