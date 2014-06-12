90ca5492dc2a08f0493dda3df0292ccdb1d67f3f
joshr2121 <joshr2121@gmail.com>
2014-06-12 16:52:04 -0400

    Audio

---
+++ CH_Week1/Assets/Scripts/TrainWhistle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrainWhistle : MonoBehaviour {
+
+	public float whistleInterval;
+	public float lastWhistleTime;
+	public bool whistling;
+
+	public AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+		whistleInterval = 30f;
+		whistling = true;
+		lastWhistleTime = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (whistling && (Time.time - whistleInterval > lastWhistleTime)) { 
+			audio.Play ();
+			lastWhistleTime = Time.time;
+		}
+	}
+}
