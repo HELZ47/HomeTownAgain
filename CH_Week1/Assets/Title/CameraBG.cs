3e88074fac30e2f22d239917a1912d0a5b259b02
Winnie Song <w.jy.song@gmail.com>
2014-06-12 13:10:26 -0400

    BuildingTest.unity - models & lighting

--- CH_Week1/Assets/Title/CameraBG.cs
+++ CH_Week1/Assets/Title/CameraBG.cs
@@ -1,15 +1,15 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class CameraBG : MonoBehaviour {
     public Color color1 = Color.red;
     public Color color2 = Color.blue;
-    public float duration = 10.0F;
+    public float duration = 15.0F;
     void Update() {
         float t = Mathf.PingPong(Time.time/5, duration) / duration;
         camera.backgroundColor = Color.Lerp(color1, color2, t);
     }
-    // void Example() {
-    //     camera.clearFlags = CameraClearFlags.SolidColor;
-    // }
+    void Example() {
+        camera.clearFlags = CameraClearFlags.SolidColor;
+    }
 }
